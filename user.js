4d38568f6c6878073f5e8a2d8585d39782c45e78
GodieGun <diego_sc25@outlook.com>
2021-06-19 13:53:15 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,2 +1,3 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
 user_pref("svg.context-properties.content.enabled", true); 
+user_pref("layout.css.color-mix.enabled", true);
