560b15e9e78abf948eaadcca1ee8dac3b9bdece0
GodieGun <diego_sc25@outlook.com>
2021-10-06 10:23:43 -0500

    Add files via upload

---
+++ Nightly/user.js
@@ -0,0 +1,4 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
+user_pref("svg.context-properties.content.enabled", true); 
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
