160ccef6950a5e804fdeb42da509c11245eb4e17
GodieGun <diego_sc25@outlook.com>
2021-10-23 23:31:42 -0500

    Delete Nightly directory

--- Nightly/user.js
+++
@@ -1,4 +0,0 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // default is false
-user_pref("svg.context-properties.content.enabled", true); 
-user_pref("layout.css.color-mix.enabled", true);
-user_pref("browser.tabs.delayHidingAudioPlayingIconMS", 0);
